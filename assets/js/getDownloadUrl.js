98701087b132b02da48e06903749d3e2451a1f23
wesleyJia <wesleyjia1105@gmail.com>
2017-09-22 23:49:39 +0800

    add get download url

--- assets/js/getDownloadUrl.js
+++ assets/js/getDownloadUrl.js
@@ -1,9 +1,16 @@
 (function () {
   $.ajax({
-    url: 'http://angulardoc.io/api/repos/showcases',
+    url: 'http://34.211.205.168/copilot/download/win32/latest',
     type: 'get',
     success: function (data) {
-      
+      console.log(data);
     }
-  })
+  });
+  $.ajax({
+    url: 'http://34.211.205.168/copilot/download/darwin/latest',
+    type: 'get',
+    success: function (data) {
+      console.log(data);
+    }
+  });
 })();
